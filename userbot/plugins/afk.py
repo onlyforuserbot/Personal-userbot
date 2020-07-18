ad531f7b5ddd605225fd64cd50f78592851fa2cc
onlyforuserbot <66107206+onlyforuserbot@users.noreply.github.com>
2020-07-18 20:22:36 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -105,7 +105,7 @@ async def on_afk(event):
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
         message_to_reply = f"__My Master Has Been In afk For__ `{total_afk_time}`\nWhere He Is: ONLY GOD KNOWS " + \
-            f"\n\n__I promise He'll back in a few light years__\n**REASON**: {reason}" \
+            f"\n\n__I promise He'll back in a few light years__\n**👉🏻REASON**: {reason}" \
             if reason \
             else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
         msg = await event.reply(message_to_reply)
@@ -143,7 +143,7 @@ async def _(event):
         if reason:
             await borg.send_message(event.chat_id, f"**I shall be Going afk!** __because ~ {reason}__")
         else:
-            await borg.send_message(event.chat_id, f"**I am Going afk!**")
+            await borg.send_message(event.chat_id, f"**🚶🚶I am Going afk!**")
         await asyncio.sleep(5)
         await event.delete()
         try:
